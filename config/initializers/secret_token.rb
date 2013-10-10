98db904d4c2c9a25cc98a4192c734f9df4c77800
Lucas Eggers <l.eggers91@gmail.com>
2013-10-09 19:24:29 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Artemix::Application.config.secret_key_base = 'd4ca27f2d9b3849e7d774bf3d0b3b100f6d397e3497da42d85269ebb5bff90ab9e47abfffe29211ea397e004f1158c497655c45e08a284ea189d69d06ec13dd6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Artemix::Application.config.session_store :cookie_store, key: '_artemix_session'
