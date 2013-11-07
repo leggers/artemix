f4b4bc339422de313f7ff969166e3571f8cdccbe
Lucas Eggers <l.eggers91@gmail.com>
2013-11-07 12:45:23 -0800

    ditched name requirement for artwork, added artist and attribution fields

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,8 +1,8 @@
 class Artwork < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>", :canvas => "300x660>" }, :default_url => "/images/:style/missing.png"
 
-  validates_presence_of :name
-  validates_uniqueness_of :name, :case_sensitive => false
+  # validates_presence_of :name
+  # validates_uniqueness_of :name, :case_sensitive => false
   validates_inclusion_of :locked, :in => [true, false]
 
   validates_attachment_presence :image
