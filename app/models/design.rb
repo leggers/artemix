21df11e5fa4d48198183e123876841d3b85fbfd9
Lucas Eggers <l.eggers91@gmail.com>
2013-11-07 18:17:19 -0800

    started on waistband text

--- app/models/design.rb
+++ app/models/design.rb
@@ -9,6 +9,7 @@ class Design < ActiveRecord::Base
   has_many :artworks, :through => :transforms
 
   def fabricate!
+    # put images on legs
     files = []
     img = Magick::ImageList.new( Artwork.find_by_name('template').image.path )
 
@@ -18,9 +19,33 @@ class Design < ActiveRecord::Base
       img = Magick::ImageList.new(img_path)
     end
 
+    # put text on waistband
+    right_leg_angle = 12.3 # degrees
+    left_leg_angle = -right_leg_angle
+
+
     self.image = File.open(img.filename)
     save!
 
     files.each {|f| File.delete(f) if File.exists?(f)}
+  end
+
+  def artist_string
+    base = "Artwork by "
+    self.artworks.each { |a| base << a.artist.strip + " and " }
+    base[0...-5]
+  end
+
+  def desginer_string
+    "Designed by " + self.designer.strip
+  end
+
+  def name_string
+    "#" + self.name.titleize.gsub(/\s+/, '')
+  end
+
+  def artist?
+    self.artworks.each { |a| return true unless a.artist.empty? }
+    return false
   end
 end
