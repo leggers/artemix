ecc9f010fa9fb323dc97468693dc6b09b94f4ad7
Lucas Eggers <l.eggers91@gmail.com>
2013-11-11 10:39:39 -0800

    refactor waistband text

--- app/models/design.rb
+++ app/models/design.rb
@@ -19,8 +19,28 @@ class Design < ActiveRecord::Base
       img = Magick::ImageList.new(img_path)
     end
 
-    # put text on waistband
+    img = self.write_waistband_text(img)
+    last_intermediate = "#{Dir.tmpdir}/#{SecureRandom.hex}.png"
+    img.write(last_intermediate)
+
+    files << last_intermediate
+
+    self.image = File.open(last_intermediate)
+    save!
+
+    files.each {|f| File.delete(f) if File.exists?(f)}
+  end
+
+  private
+
+  def write_waistband_text img
     # TODO: compensate for name length by shrinking and repositioning text
+    img = write_left_waistband_text img
+    img = write_right_waistband_text img
+    img
+  end
+
+  def write_left_waistband_text img
     left_leg_angle = 12.3 # degrees
     left_text = Magick::Draw.new
     left_text.rotate(left_leg_angle)
@@ -30,7 +50,10 @@ class Design < ActiveRecord::Base
     left_text.fill = 'white'
     left_text.text(3000, 565, self.name_string)
     left_text.draw(img)
+    img
+  end
 
+  def write_right_waistband_text img
     right_leg_angle = -left_leg_angle
     right_text = Magick::Draw.new
     right_text.rotate(right_leg_angle)
@@ -40,15 +63,7 @@ class Design < ActiveRecord::Base
     right_text.text(6850, 3000, self.artist_string)
     right_text.text(6850, 3160, self.desginer_string)
     right_text.draw(img)
-
-    last_intermediate = "#{Dir.tmpdir}/#{SecureRandom.hex}.png"
-    img.write(last_intermediate)
-    files << last_intermediate
-
-    self.image = File.open(last_intermediate)
-    save!
-
-    files.each {|f| File.delete(f) if File.exists?(f)}
+    img
   end
 
   def artist_string
