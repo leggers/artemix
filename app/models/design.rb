2ae3a606856e9d168886114c07b0a21e6bb24013
Lucas Eggers <l.eggers91@gmail.com>
2013-11-11 12:50:18 -0800

    left text resizing good

--- app/models/design.rb
+++ app/models/design.rb
@@ -1,7 +1,7 @@
 class Design < ActiveRecord::Base
   has_attached_file :image
 
-  validates_presence_of :name
+  validates_presence_of :name # 24 character soft upper limit for visual reasons
   validates_uniqueness_of :name, :case_sensitive => false
   validates_presence_of :designer
 
@@ -31,7 +31,7 @@ class Design < ActiveRecord::Base
     files.each {|f| File.delete(f) if File.exists?(f)}
   end
 
-  private
+  # private
 
   def write_waistband_text img
     # TODO: compensate for name length by shrinking and repositioning text
@@ -41,29 +41,48 @@ class Design < ActiveRecord::Base
   end
 
   def write_left_waistband_text img
-    left_leg_angle = 12.3 # degrees
-    left_text = Magick::Draw.new
-    left_text.rotate(left_leg_angle)
-    left_text.font_family = 'helvetica'
-    left_text.font_weight(700)
-    left_text.pointsize = 300
-    left_text.fill = 'white'
-    left_text.text(3000, 565, self.name_string)
-    left_text.draw(img)
+    text = self.name_string
+    left_waistband_angle = 12.3 # degrees
+    text_writer = Magick::Draw.new
+    text_writer.rotate(left_waistband_angle)
+    text_writer.font_family = 'helvetica'
+    text_writer.fill = 'white'
+    text_writer.font_weight(700)
+
+    text_x = 3000
+    text_y = 565
+    pointsize = 300
+
+    offset_coefficient = text.length - 11
+    if offset_coefficient > 0
+      text_x -= self.max_x_offset(offset_coefficient) * 75
+      text_y -= offset_coefficient * 1.5
+      if offset_coefficient > 2
+        pointsize -= offset_coefficient * 9
+      end
+    end
+
+    text_writer.pointsize = pointsize
+    text_writer.text(text_x, text_y, text)
+    text_writer.draw(img)
     img
   end
 
   def write_right_waistband_text img
     right_leg_angle = -left_leg_angle
-    right_text = Magick::Draw.new
-    right_text.rotate(right_leg_angle)
-    right_text.font_family = 'helvetica'
-    right_text.pointsize = 150
-    right_text.fill = 'white'
-    right_text.text(6850, 3000, self.artist_string)
-    right_text.text(6850, 3160, self.desginer_string)
-    right_text.draw(img)
+    text_writer = Magick::Draw.new
+    text_writer.rotate(right_leg_angle)
+    text_writer.font_family = 'helvetica'
+    text_writer.pointsize = 150
+    text_writer.fill = 'white'
+    text_writer.text(6850, 3000, self.artist_string)
+    text_writer.text(6850, 3160, self.desginer_string)
+    text_writer.draw(img)
     img
+  end
+
+  def max_x_offset offset_coefficient
+    offset_coefficient >= 5 ? 5 : offset_coefficient
   end
 
   def artist_string
