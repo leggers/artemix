8bb4bfcd65d921e2b13e27bf715668fbeb06d192
Lucas Eggers <l.eggers91@gmail.com>
2013-11-22 15:16:50 -0800

    added more transform specs

--- spec/models/transform_spec.rb
+++ spec/models/transform_spec.rb
@@ -14,7 +14,7 @@ describe Transform do
     :width => '1',
     :rotation => "#{Math::PI / 2}"
   } }
-  let(:valid_numbers) { {
+  let(:valid_attributes) { {
     :image_x => 1,
     :image_y => 1,
     :height => 1,
@@ -22,7 +22,13 @@ describe Transform do
     :rotation => Math::PI / 2,
     :artwork => @artwork,
     :leg => "left"
-  } } 
+  } }
+  let(:valid_negative_strings) { {
+    :image_x => '-2',
+    :image_y => '-1',
+    :height => '4',
+    :width => '5'
+  } }
   before do
     @artwork = Artwork.create!(artwork_attributes)
   end
@@ -30,54 +36,70 @@ describe Transform do
     it 'should convert to numbers' do
       t = Transform.new(valid_strings)
       t.prepare_for_create
-      t.image_x.should eq(valid_numbers[:image_x])
-      t.image_y.should eq(valid_numbers[:image_y])
-      t.height.should eq(valid_numbers[:height])
-      t.width.should eq(valid_numbers[:width])
-      t.rotation.should eq(valid_numbers[:rotation])
+      t.image_x.should eq(valid_attributes[:image_x])
+      t.image_y.should eq(valid_attributes[:image_y])
+      t.height.should eq(valid_attributes[:height])
+      t.width.should eq(valid_attributes[:width])
+      t.rotation.should eq(valid_attributes[:rotation])
+    end
+    it 'should convert negative numbers properly' do
+      t = Transform.new(valid_negative_strings)
+      t.prepare_for_create
+      t.image_x.should eq(-2)
+      t.image_y.should eq(-1)
+      t.height.should eq(4)
+      t.width.should eq(5)
     end
   end
   describe 'with proper values' do
     it 'should save' do
-      Transform.create!(valid_numbers)
+      Transform.create!(valid_attributes)
     end
   end
   describe 'without proper values' do
     it 'should not save without image_x' do
-      valid_numbers.delete(:image_x)
-      expect{ Transform.create!(valid_numbers) }.to raise_error
+      valid_attributes.delete(:image_x)
+      expect{ Transform.create!(valid_attributes) }.to raise_error
     end
     it 'should not save without image_y' do
-      valid_numbers.delete(:image_y)
-      expect{ Transform.create!(valid_numbers) }.to raise_error
+      valid_attributes.delete(:image_y)
+      expect{ Transform.create!(valid_attributes) }.to raise_error
     end
     it 'should not save without height' do
-      valid_numbers.delete(:height)
-      expect{ Transform.create!(valid_numbers) }.to raise_error
+      valid_attributes.delete(:height)
+      expect{ Transform.create!(valid_attributes) }.to raise_error
     end
     it 'should not save without width' do
-      valid_numbers.delete(:width)
-      expect{ Transform.create!(valid_numbers) }.to raise_error
+      valid_attributes.delete(:width)
+      expect{ Transform.create!(valid_attributes) }.to raise_error
     end
     it 'should not save without an artwork' do
-      valid_numbers.delete(:artwork)
-      expect{ Transform.create!(valid_numbers) }.to raise_error
+      valid_attributes.delete(:artwork)
+      expect{ Transform.create!(valid_attributes) }.to raise_error
     end
     it 'should not save without a leg specified' do
-      valid_numbers.delete(:leg)
-      expect{ Transform.create!(valid_numbers) }.to raise_error
+      valid_attributes.delete(:leg)
+      expect{ Transform.create!(valid_attributes) }.to raise_error
     end
   end
   describe 'scaling' do
     it 'should change values when scaled' do
-      t = Transform.new(valid_numbers)
+      t = Transform.new(valid_attributes)
       t.scale
-      t.image_x.should eq(valid_numbers[:image_x] * 20)
-      t.image_y.should eq(valid_numbers[:image_y] * 20)
-      t.height.should eq(valid_numbers[:height] * 20)
-      t.width.should eq(valid_numbers[:width] * 20)
-      p valid_numbers[:rotation]
-      t.rotation.should eq(valid_numbers[:rotation] * 180 / Math::PI)
+      t.image_x.should eq(valid_attributes[:image_x] * 20)
+      t.image_y.should eq(valid_attributes[:image_y] * 20)
+      t.height.should eq(valid_attributes[:height] * 20)
+      t.width.should eq(valid_attributes[:width] * 20)
+      t.rotation.should eq(valid_attributes[:rotation] * 180 / Math::PI)
+    end
+    it 'should scale negative numbers properly' do
+      t = Transform.new(valid_negative_strings)
+      t.prepare_for_create
+      t.scale
+      t.image_x.should eq(-40)
+      t.image_y.should eq(-20)
+      t.height.should eq(80)
+      t.width.should eq(100)
     end
   end
 end
