f8ce2dea58cc143ac49385fe85da8e06c42858de
Lucas S Eggers <l.eggers91@gmail.com>
2014-05-26 18:20:29 -0300

    model is ready to use, almost

---
+++ config/deploy/develop.rb
@@ -0,0 +1,7 @@
+set :stage, :develop
+set :rails_env, 'production'
+set :migration_role, 'db'
+
+role :app, %w{deploy@titica.bombsheller.com}
+role :web, %w{deploy@titica.bombsheller.com}
+role :db, %w{deploy@titica.bombsheller.com}
